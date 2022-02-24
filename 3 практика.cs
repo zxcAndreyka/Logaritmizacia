7eb0aa9f711c49ab99e0daf121e558657561ef8f
Truncklin <100361668+Truncklin@users.noreply.github.com>
2022-02-24 20:08:30 +0300

    Create 3 практика.cs

---
+++ 3 практика.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 задание 
+            /*Console.WriteLine("Напишите поочередно координаты коня.Сначала координаты по Х, потом по У");
+            int X1 = Convert.ToInt32(Console.ReadLine());
+            int Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Напишите поочередно координаты пешки.Сначала координаты по Х, потом по У");
+            int X2 = Convert.ToInt32(Console.ReadLine());
+            int Y2 = Convert.ToInt32(Console.ReadLine());
+            int q = X1 + 2;
+            int w = Y1 + 1;
+            if (((q == X2) & (w == Y2)) | ((q == X2) & (Y1 - 1 == Y2)) | ((X1 + 1 == X2) & (Y1 + 2 == Y2)) | ((X1 + 1 == X2) & (Y1 - 2 == Y2))
+            | ((X1 - 2 == X2) & (Y1 - 1 == Y2)) | ((X1 - 2 == X2) & (Y1 + 1 == Y2)) | ((X1 - 1 == X2) & (Y1 - 2 == Y2)) | ((X1 - 1 == X2) & (Y1 + 2 == Y2)))
+            {
+                Console.WriteLine("Конь может съесть пешку");
+            }
+            else if (X1 > 8 | Y1 > 8 | X2 > 8 | Y2 > 8)
+                Console.WriteLine("Вы написали координаты за границей шахматной доски");
+            else
+                Console.WriteLine("конь не может съесть пешку");*/
+
+            // 2 задание
+            /*Console.WriteLine("Введите возраст ");
+              int V = Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("Введите пол (мужской/женский)");
+              string P = Console.ReadLine();
+              int K;
+              if(P == "женский")
+              {
+              K = 2 * V - 14;
+              }
+              else
+              {
+              K = V / 2 + 7;
+              }
+              Console.WriteLine("Лучший возраст вашего партнёра ="+ K);*/
+
+            // 3 задание
+            /* Console.WriteLine("Введите натуральное число ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            if(x%2==0)
+            {
+            Console.WriteLine("число чётное");
+            }
+            else
+            {
+            Console.WriteLine("число нечётное");
+            }*/
+
+            // 4 задание
+            /*Console.WriteLine("Введите натуральное число ");
+            int F = Convert.ToInt32(Console.ReadLine());
+            int g = F / 2;
+            double n = Convert.ToDouble(F);
+            double l = n / 2;
+            if (g < l)
+            {
+                Console.WriteLine(" число не четное ");
+            }
+            else
+            {
+                Console.WriteLine(" число четное ");
+            }
+            Console.ReadKey();*/
+
+
+
+
+        }
+
+    }
+}
