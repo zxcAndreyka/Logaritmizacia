e9ef09047a89a448a03ce96bc7b9471fd3cd250c
Truncklin <100361668+Truncklin@users.noreply.github.com>
2022-02-25 10:54:49 +0300

    Create 5 практика.cs

---
+++ 5 практика.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1 задание 
+            // 1 задание 
+            /*int a = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int k = 0;
+            do
+            {
+               count ++;
+                k += count;
+            }
+            while(count < a );
+            Console.WriteLine(k / 5);
+            Console.ReadKey();*/
+
+            //2 Задание
+        }
+    }
+}
