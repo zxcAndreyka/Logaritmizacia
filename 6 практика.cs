b928dbcb440801e7749291ef3386478047bb0b20
Truncklin <100361668+Truncklin@users.noreply.github.com>
2022-02-25 10:38:10 +0300

    5 практика

--- 6 практика.cs
+++ 6 практика.cs
@@ -11,115 +11,19 @@ namespace _1
         static void Main(string[] args)
         {
             // 1 задание 
-            /*Console.WriteLine("Введите радиус круга");
-            double r = Convert.ToDouble(Console.ReadLine());
-            int g = 10;
+            int a = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int k = 0;
             do
             {
-                Console.WriteLine("напишите координаты выстрела по Х");
-                double x = Convert.ToDouble(Console.ReadLine());
- 
-                Console.WriteLine("напишите координаты выстрела по Y");
-                double y = Convert.ToDouble(Console.ReadLine());
-                double xr = -r;
-                double r1 = r;
-                double yr = r1;
-                if ((((x <= (xr + r)) || (x <= (xr - r))) && ((y <= (yr + r)) || (y <= (yr - r)))))
-                {
-                    Console.WriteLine("попал");
-                }
-                else
-                {
-                    Console.WriteLine("не попал");
-                }
-                g--;
+               count ++;
+                k += count;
+                Console.WriteLine(k);
             }
-            while (g!=0);*/
-
-            // 2
-
-            /*double count = 0;
-            double i = 3;
- 
-
-                do
-                {
-                    i -= 2;
-                    count++;
-                } while (count <= 9);
-                Console.WriteLine(i);
-
-
-            double a = double.Parse(Console.ReadLine());
-            double count1 = 3;
-            for(int j =0; j < 9; j++)
-            {
-                count1 = (Math.Pow(count1, 2) - a) * count1;
-            }
-                Console.WriteLine(count1);
-            
-            Console.ReadKey();*/
-
-            // 3
-            /*int n = 100;
-            int max = 0;
-            do
-            {
-                int s = 0;
-                int a1 = n / 100;
-                int a2 = (n / 10) % 10;
-                int a3 = n % 10;
-                s = a1 + a2 + a3;
-                int t = n / s;
-                if (t > max)
-                {
-                    max = t;
-                    n = n + 1;
-                }
-                else
-                {
-                    n = n + 1;
-                }
-            }
-            while (n < 1000);
-            Console.WriteLine(max);
-            Console.ReadKey();*/
-
-            // 4
-            /*            Console.WriteLine("Напишите число");
-
-                        int n = Convert.ToInt32(Console.ReadLine());
-                        int delenie = n % 2;
-                        int g = 1;
-                        int count = 0;
-
-                        if (delenie == 0)
-                        {
-
-                            do
-                            {
-                                count += 2;
-                                g *= count;
-
-                            }
-                            while (count <= n);
-                            Console.WriteLine(g);
-                        }
-                        else
-                        {
-                            count = 1;
-                            do
-                            {
-                                g *= count;
-                                count += 2;
-
-
-                            }
-                            while (count <= n);
-                            Console.WriteLine(g);
-
-                        }*/
+            while(count < a );
+            Console.WriteLine(k / 5);
             Console.ReadKey();
+
         }
     }
 }
