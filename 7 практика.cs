a39f6e872f6bb62d462d63a9ec9e5bd006a6326b
Truncklin <100361668+Truncklin@users.noreply.github.com>
2022-03-01 12:51:49 +0300

    Add files via upload

---
+++ 7 практика.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1 задание 
+            Console.WriteLine("введите n");
+            Double n = Convert.ToDouble(Console.ReadLine());
+            int g = 0;
+            int h = 1;
+            for (int count = 1; n >= count; count ++)
+            {
+                h += 2;
+              g = g + h;
+            }
+            Console.WriteLine(g);
+
+
+        }
+    }
+}
