321d0d5e24fea2c8399d3810620b14220da92a2e
Truncklin <100361668+Truncklin@users.noreply.github.com>
2022-03-03 08:50:41 +0300

    Update 7 практика.cs

--- 7 практика.cs
+++ 7 практика.cs
@@ -16,7 +16,7 @@ namespace _1
             Double n = Convert.ToDouble(Console.ReadLine());
             int g = 0;
             int h = 1;
-            for (int count = 1; n >= count; count++)
+            for (int r = 1; n >= r; r++)
             {
                 h += 2;
                 g = g + h;
@@ -26,7 +26,6 @@ namespace _1
             // 3 задание
             /*Random x = new Random();
             int n = x.Next(1,11);
-            Console.WriteLine(n);
             Console.WriteLine("Попробуйте угадать число от 1-10, у вас есть 5 попыток");
             {
                 for(int t = 0; t < 5; t++)
@@ -44,14 +43,15 @@ namespace _1
                         Console.WriteLine("Попробуйте снова");
                 }
             }*/
-
+            
+            // 4 задание
             int n = Convert.ToInt32(Console.ReadLine());
             n = 2 * n + 1;
             double y = 0;
             y = Math.Sqrt(n); 
-            for (int count = n - 2; count >= 1; count -= 2)
+            for (int r = n - 2; r >= 1; r -= 2)
             {
-                y = Math.Sqrt(count + y);
+                y = Math.Sqrt(r + y);
             }
             Console.WriteLine(y);   
 
